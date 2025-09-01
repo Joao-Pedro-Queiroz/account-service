1ef5de3c678174a4ce6f450cbae918ead3b76892
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2025-09-01 20:48:46 -0300

    Service criado

---
+++ src/main/java/store/account/AccountService.java
@@ -0,0 +1,28 @@
+package store.account;
+
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account create(Account account) {
+        return accountRepository.save(
+            new AccountModel(account)
+        ).to();
+    }
+
+    public List<Account> findAll() {
+        return StreamSupport.stream(
+            accountRepository.findAll().spliterator(), false)
+            .map(AccountModel::to)
+            .toList();
+    }
+
+}
